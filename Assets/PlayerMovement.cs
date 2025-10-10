3243f9ed920e990612a21c602ea393b119aea30d
MUKILTEO\1549673 <apollorevermann@gmail.com>
2025-10-10 13:07:23 -0700

    Made movement and jump mechanics but the player and camera can rotate

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -12,6 +12,17 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+      if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            myRigidbody.linearVelocity = Vector2.up * 10;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) == true)
+        {
+            myRigidbody.linearVelocity = Vector2.left * 10;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) == true)
+        {
+            myRigidbody.linearVelocity = Vector2.right * 10;
+        }
     }
 }
