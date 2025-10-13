79bc433fdd063ec4e8b263358d4ae8813aaad817
MUKILTEO\1549673 <apollorevermann@gmail.com>
2025-10-13 13:22:51 -0700

    Made speed increas without collision mess up

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
+    public float movespeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,21 +14,22 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      if (Input.GetKeyDown(KeyCode.UpArrow) == true)
+      if (Input.GetKeyDown(KeyCode.UpArrow) == true )
         {
-            myRigidbody.linearVelocityY = 10;
+            myRigidbody.linearVelocityY = movespeed;
         }
         if (Input.GetKeyDown(KeyCode.LeftArrow) == true)
         {
-            myRigidbody.linearVelocityX = -10;
+            myRigidbody.linearVelocityX = -movespeed;
         }
         if (Input.GetKeyDown(KeyCode.RightArrow) == true)
         {
-            myRigidbody.linearVelocityX = 10;
+            myRigidbody.linearVelocityX = movespeed;
         }
         if (Input.GetKeyDown(KeyCode.DownArrow) == true)
         {
-            myRigidbody.linearVelocityY = -10;
+            myRigidbody.linearVelocityY = -movespeed;
         }
+
     }
 }
