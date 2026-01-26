c34ddf6a7bac0c256083d9396cef954565015771
MUKILTEO\1549673 <1549673>
2026-01-26 12:55:04 -0800

    move testing

--- Assets/PLayer_Movement.cs
+++ Assets/PLayer_Movement.cs
@@ -7,11 +7,11 @@ public class PLayer_Movement : MonoBehaviour
     public float movelength_L;
     public float movespeed_R;
     public float movelength_R;
-    public float jumpspeed_J;
+    public float jumptime_J;
     public float jumpheight_J;
     public float Groundspeed_G;
     public float Gravityspeed_G;
-    public float variableYspeed_VVV;
+    public float jumptimeleft_J;
     //dashing
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -28,18 +28,18 @@ public class PLayer_Movement : MonoBehaviour
         //Jumping
         if ((Input.GetKeyDown(KeyCode.UpArrow) == true || Input.GetKeyDown(KeyCode.W) == true) && (GetComponent<Detection>().Detection_D == true))
         {
-            variableYspeed_VVV = jumpheight_J;
+            jumptimeleft_J = jumptime_J;
             GetComponent<Detection>().Detection_D = false;
         }
-        if (variableYspeed_VVV < 0)
+        if (jumptimeleft_J < 0)
         {
-            variableYspeed_VVV = 0;
+            jumptimeleft_J = 0;
         }
-        if (variableYspeed_VVV > 0)
+        if (jumptimeleft_J > 0)
         {
-            variableYspeed_VVV -= jumpspeed_J * Time.deltaTime * 0.5f;
-            transform.position += Vector3.up * variableYspeed_VVV * Time.deltaTime;
-            variableYspeed_VVV -= jumpspeed_J * Time.deltaTime * 0.5f;
+            jumptimeleft_J -= Time.deltaTime * 1f;
+            transform.position += Vector3.up * jumpheight_J / jumptime_J * Time.deltaTime + Physics2D.gravity;
+            
         }
         //Grounding
         if (Input.GetKeyDown(KeyCode.DownArrow) == true || Input.GetKeyDown(KeyCode.S) == true)
