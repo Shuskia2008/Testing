724f6ae39a66c8bc12d77e7b6c75346049bf9fa9
MUKILTEO\1549673 <1549673>
2026-02-20 12:38:06 -0800

    some trues and falses were corrected to what they were supposed to be.

--- Assets/Player_Movement_1_0_2.cs
+++ Assets/Player_Movement_1_0_2.cs
@@ -3,49 +3,70 @@ using UnityEngine.InputSystem;
 
 public class Player_Movement_1_0_2 : MonoBehaviour
 {
+    #region Variables
+    #region PlayerBody_B
+    //Body
     public Rigidbody2D myRigidbody;
+    #endregion
+    #region Moving_M
+    #region Moving_M
     //Moving
     public float moveX;
     public float movecontroller;
+    #endregion
+    #region Moving_L
     //-Left-
     public float movespeed_L;
     public float movelength_L;
     public bool canMoving_L;
+    #endregion
+    #region Moving_R
     //-Right-
     public float movespeed_R;
     public float movelength_R;
     public bool canMoving_R;
+    #endregion
+    #endregion
+    #region Jumping_J
     //Jumping
     public float jumptime_J;
     public float jumptimeleft_J;
-
     public float jumpheight_J;
     public bool canJumping_J;
+    #endregion
+    #region Grounding_G
     //Grounding
+    #region Gravity_G
+    //-Gravity-
+    public float Gravityspeed_G;
+    public float GravityAngle_G;
+    public float GDx;
+    public float GDy;
+    #endregion
+    #region GroundPounding_G
     //-Groundpound-
     public float Groundspeed_G;
     public bool canGrounding_G;
-    //-Gravity-
-    public float Gravityspeed_G;
+    #endregion
+    #endregion
+    #region Dashing_D
     //Dashing
-    public float GDx;
-    public float GDy;
-
-
+    #endregion
+    #endregion
     #region Player
     void Update()
     {
         #region Moving_M
 
-        if ((GetComponent<Detection>().Detection_R == false) && (movecontroller > 0) && (canMoving_L == false) && (canMoving_R == false))
+        if ((GetComponent<Detection>().Detection_R == false) && (movecontroller > 0) && (canMoving_R == true))
         {
             moveX = movecontroller * movespeed_L;
         }
-        else if ((GetComponent<Detection>().Detection_L == false) && (movecontroller < 0) && (canMoving_L == false) && (canMoving_R == false))
+        else if ((GetComponent<Detection>().Detection_L == false) && (movecontroller < 0) && (canMoving_L == true))
         {
             moveX = movecontroller * movespeed_R;
         }
-        else if ((movecontroller == 0) && (canMoving_L == false) && (canMoving_R == false))
+        else if ((movecontroller == 0) && (canMoving_L == true) && (canMoving_R == true))
         {
             moveX = movecontroller;
         }
@@ -80,15 +101,17 @@ public class Player_Movement_1_0_2 : MonoBehaviour
         //myRigidbody.linearVelocityY = GDy;
         //myRigidbody.linearVelocityY -= Gravityspeed_G * Time.deltaTime;
         Physics2D.gravity = new Vector2(0, 0);
-        float angle = 90;
-        float distance = 1;
-        Vector2 dir = new Vector2(Mathf.Sin(angle), Mathf.Cos(angle)) * distance;
-
-        # endregion
+        //float angle = 90;
+        //float distance = 1;
+        //Vector2 dir = new Vector2(Mathf.Sin(angle), Mathf.Cos(angle)) * distance;
+        float angle = GravityAngle_G * Mathf.Deg2Rad;
+        float distance = Gravityspeed_G;
+        Vector3 dir = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * distance;
+        #endregion
         #region GroundPounding_G
         //Grounding
-    #endregion
-    #endregion
+        #endregion
+        #endregion
     }
     #endregion
     #region Controller
@@ -101,12 +124,12 @@ public class Player_Movement_1_0_2 : MonoBehaviour
     #region Jumping Controller_J
     public void Jump(InputAction.CallbackContext ctx)
     {
-            if ((ctx.started) && (GetComponent<Detection>().Detection_D == true) && (canJumping_J == false))
+            if ((ctx.performed) && (GetComponent<Detection>().Detection_D == true) && (canJumping_J == true))
             {
                 jumptimeleft_J = jumptime_J;
                 GetComponent<Detection>().Detection_D = false;
             }
-            else if ((ctx.canceled) && (canJumping_J == false))
+            else if ((ctx.canceled) && (canJumping_J == true))
             {
                 jumptimeleft_J = 0;
             }
@@ -120,8 +143,8 @@ public class Player_Movement_1_0_2 : MonoBehaviour
         Gizmos.DrawLine(transform.position, transform.position + (Vector3)myRigidbody.linearVelocity);
 
 
-        float angle = 90 * Mathf.Deg2Rad;
-        float distance = 1;
+        float angle = GravityAngle_G * Mathf.Deg2Rad;
+        float distance = Gravityspeed_G;
         Vector3 dir = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * distance;
 
         Gizmos.color = Color.blue;
@@ -131,7 +154,7 @@ public class Player_Movement_1_0_2 : MonoBehaviour
     #region GroundPounding Controller_G
     public void GroundPounding(InputAction.CallbackContext ctx)
     {
-        if (ctx.ReadValue<Vector2>().y == -1 && canGrounding_G == true)
+        if (ctx.performed && canGrounding_G == true && (GetComponent<Detection>().Detection_D == false))
         {
             myRigidbody.linearVelocityY = -Groundspeed_G;
         }
