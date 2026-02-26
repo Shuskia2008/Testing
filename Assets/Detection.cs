fc246566da6e9b35b238b0e73cff151143ed06d3
MUKILTEO\1549673 <1549673>
2026-02-26 13:18:34 -0800

    all scripts in player are now comments

--- Assets/Detection.cs
+++ Assets/Detection.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Detection : MonoBehaviour
 {
+    /*
     public bool isParent;
 
     public bool Detection_L = false;
@@ -66,5 +67,5 @@ public class Detection : MonoBehaviour
         {
             Detection_D = false;
         }
-    }
+    }*/
 }
