cde8d6925ded91151a381ee71949472479b8b330
MUKILTEO\1549673 <1549673>
2026-03-03 12:58:22 -0800

    planet gravity attraction stuuf

---
+++ Assets/Attractor.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Attractor : MonoBehaviour
+{
+    public LayerMask AttractionLayer;
+    public float Gravity = -10;
+    [SerializeField] private float effectionRadius = 10;
+    public List<Collider2D> AttractedObjects = new List<Collider2D>();
+    [HideInInspector] public Transform planetTransform;
+    void Awake()
+    {
+        planetTransform = GetComponent<Transform>();
+    }
+    void Update()
+    {
+        SetAttractedObjects();
+    }
+    void FixedUpdate()
+    {
+        AttractObjects();
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, effectionRadius);
+    }
+    void SetAttractedObjects()
+    {
+        AttractedObjects = Physics2D.OverlapCircleAll(transform.position, effectionRadius, AttractionLayer).ToList();
+    }
+    void AttractObjects()
+    {
+        for (int i = 0; i < AttractedObjects.Count; i++)
+        {
+            AttractedObjects[i].GetComponent<Attractable>().Attract(this);
+        }
+    }
+}
