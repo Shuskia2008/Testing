0148113f47a5112c1377a7badfd1a9e2353d8452
MUKILTEO\1549673 <1549673>
2026-04-17 13:15:13 -0700

    still fixing stuff

--- Assets/Item.cs
+++ Assets/Item.cs
@@ -16,7 +16,7 @@ public class Item : MonoBehaviour
         inventoryManager = GameObject.Find("InventoryCanvas").GetComponent<InventoryManager>();
         GetComponentInChildren<SpriteRenderer>().sprite = sprite;
     }
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
